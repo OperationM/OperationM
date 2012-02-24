4ad848924a29fc5ddc2a6d4ccbca32bcefb06eb9
Ken Fukuyama <ken.fukuyama@gmail.com>
2012-02-25 01:49:49 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoogleApp::Application.config.secret_token = 'b212a28157348d3243339979c24efce711a4f9203718d9fb4fa9e3735b0e400d4ac23602c21eab7d98315748516b9c56f162c8a0e918ad6cddb9c1d0843f740d'
