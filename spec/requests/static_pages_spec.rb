07c147ca588073844243c75c57a3beccab17c7aa
rynqs <cecilcat@gmail.com>
2012-02-27 22:14:35 +0900

    静的ページ追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      # get static_pages_index_path
-      # response.status.should be(200)
+  describe "GET /static_pages/home" do
+    it "should have the content 'Moogle'" do
+      visit '/static_pages/home'
+      page.should have_content('Moogle')
     end
   end
 end
