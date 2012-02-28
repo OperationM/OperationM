26209698ea7577d8f7c0e73e29c57034b9cb330c
rynqs <cecilcat@gmail.com>
2012-02-29 00:19:47 +0900

    HTMLテスト追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 MoogleApp::Application.routes.draw do
-  match '/', to: 'static_pages#home'
 
-  get "static_pages/home"
-    
-  get "static_pages/help"
+  match '/home', to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/about"
+  root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,86 @@
 require 'spec_helper'
 
+# 静的ページのHTMLテスト
 describe "StaticPages" do
-	describe "GET /" do
-	  it "should have the content 'Home'" do
-	    visit '/'
-	    page.should have_content('Home')
-	    page.should have_content('About')
-	    page.should have_content('Help')
-	    page.should have_content('Sign in')
-	  end
+	#　対象
+	subject{ page }
+
+	# ナビゲーションのHTMLテスト
+	describe "nav" do
+		# root_pathに遷移
+		before{ visit root_path }
+
+		# Homeページへのリンク配置
+		it{ should have_link 'Home', href: root_path }
+
+	  # Aboutページへのリンク配置
+	  it{ should have_link 'About', href: about_path }
+
+	  # Helpページへのリンク配置
+	  it{ should have_link 'Help', href: help_path }
 	end
 
-	describe "GET /static_pages/home" do
-	  it "should have the content 'Home'" do
-	    visit '/static_pages/home'
-	    page.should have_content('Home')
-	  end
+	# ヘッダーのHTMLテスト
+	describe "header" do
+		# root_pathに遷移
+		before{ visit root_path }
+
+		# ロゴの配置と属性の設定
+		it{ should have_selector 'img', alt: "Moogle" }
+
+		# ロゴをroot_pathにリンク
+		it{ should have_link 'Moogle', href: root_path }
+
+		# 検索用テキストフィールドの配置
+		it{ should have_selector 'input', type: "text" }
+
+		# 検索実行用のボタンの配置
+		it{ should have_selector 'input', type: "button" }
 	end
 
-	describe "GET /static_pages/about" do
-	  it "should have the content 'About'" do
-	    visit '/static_pages/about'
-	    page.should have_content('About')
-	  end
+	# フッターのHTMLテスト
+	describe "footer" do
+		# root_pathに遷移
+		before{ visit root_path }
 	end
 
-	describe "GET /static_pages/Help" do
-	  it "should have the content 'Help'" do
-	    visit '/static_pages/help'
-	    page.should have_content('Help')
-	  end
+	# 静的ページの共通
+	shared_examples_for "all static pages" do
+		# サイトタイトルにページタイトルを追加
+		it{ should have_selector 'title', text: full_title(page_title) }
+	end
+
+	# Home遷移時のHTMLテスト
+	describe "Home page" do
+		# home_pathに遷移
+		before{ visit home_path }
+
+		# ページタイトルはHome
+		let(:page_title){'Home'}
+
+		it_should_behave_like "all static pages"
+	end
+
+	# About遷移時のHTMLテスト
+	describe "About page" do
+		# about_pathに遷移
+		before{ visit about_path }
+
+		# ページタイトルはAbout
+		let(:page_title){'About'}
+
+		it_should_behave_like "all static pages"
+	end
+
+	# Help遷移時のHTMLテスト
+	describe "Help page" do
+		# help_pathに遷移
+		before{ visit help_path }
+
+		# ページタイトルはHelp
+		let(:page_title){'Help'}
+
+		it_should_behave_like "all static pages"
 	end
 
 end
