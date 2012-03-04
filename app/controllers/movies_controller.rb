2a302c2a2387ce7842bd8cf6b6ca14d7acec8a59
rynqs <cecilcat@gmail.com>
2012-03-05 00:39:55 +0900

    暫定対処個所等にコメント追加

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,44 +5,61 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     current_user = Omniuser.find(session[:user_id])
 
-    # feed更新時のpush通知に対応までの暫定対処
-    https = Net::HTTP.new("graph.facebook.com", 443)
-    https.use_ssl = true
-    https.ca_file = "#{$RAILS_ROOT}/cacert.pem"
-    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    https.verify_depth = 5
-    https.start do |w|
-      response = w.get("/387659801250930/feed?fields=id,object_id,picture,source,properties&access_token=#{current_user.access_token}")
-      feed = JSON.parse(response.body)
-      video_meta_id = Hash.new
-      feed.each do |rk, rh|
-        if rk == "data"
-          rh.each do |item|
-            if item.has_key?("object_id")
-              video_meta_id[item["object_id"]] = item["id"]
+    ###### feed更新時のpush通知に対応までの暫定対処(FeedFetch版)
+    ###### 問題点１：index.html.erbロード時にfetchするので表示が遅くなる
+    ###### 問題点２：feedの内容は一回ですべて取得できない(ページングがある)ので投稿だけすすんでだれもロードしなかった時更新が漏れる
+    # source(ビデオのURL)が設定されてないオブジェクトを取得
+    @not_yet = Movie.find_all_by_source(nil)
+    # 空じゃなかったらFBに更新されているか探しにいく
+    if !@not_yet
+      # HTTPSリクエスト用のオブジェクト用意
+      https = Net::HTTP.new("graph.facebook.com", 443)
+      https.use_ssl = true
+      # Herokuにあげる時はこれはいらないはず
+      https.ca_file = "#{$RAILS_ROOT}/cacert.pem"
+      https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      https.verify_depth = 5
+      # リクエスト開始
+      https.start do |w|
+        # Operation_M_testグループのfeedを取得
+        response = w.get("/387659801250930/feed?fields=id,object_id,picture,source,properties&access_token=#{current_user.access_token}")
+        # feedから投稿完了時に返ってきたobject_idとひもづく投稿idを取得して対応をハッシュに保持
+        feed = JSON.parse(response.body)
+        video_meta_id = Hash.new
+        feed.each do |rk, rh|
+          if rk == "data"
+            rh.each do |item|
+              if item.has_key?("object_id")
+                video_meta_id[item["object_id"]] = item["id"]
+              end
             end
           end
         end
-      end
-      logger.debug "video_meta_id: #{video_meta_id}"
-      @movies.each do |movie|
-        if video_meta_id.has_key?(movie.video)
-          response = w.get("/#{video_meta_id[movie.video]}?access_token=#{current_user.access_token}")
-          video_info = JSON.parse(response.body)
-          logger.debug "#{movie.video}->meta: #{video_meta_id[movie.video]}"
-          movie.meta = video_meta_id[movie.video]
-          logger.debug "#{movie.video}->picture: #{video_info["picture"]}"
-          movie.picture = video_info["picture"]
-          # logger.debug "#{movie.video}->source: #{video_info["source"]}"
-          # movie.source = video_info["source"]
-          logger.debug "#{movie.video}->source: http://www.facebook.com/v/#{movie.video}"
-          movie.source = "http://www.facebook.com/v/#{movie.video}"
-          logger.debug "#{movie.video}->length: #{video_info["properties"][0]["text"]}"
-          movie.length = video_info["properties"][0]["text"]
-          movie.save
+        # development.logに対応表を出力
+        logger.debug "video_meta_id: #{video_meta_id}"
+        # sourceが設定されていないmovieオブジェクトにfeedからいろいろ設定していく
+        @not_yet.each do |movie|
+          if video_meta_id.has_key?(movie.video)
+            response = w.get("/#{video_meta_id[movie.video]}?access_token=#{current_user.access_token}")
+            video_info = JSON.parse(response.body)
+            logger.debug "#{movie.video}->meta: #{video_meta_id[movie.video]}"
+            movie.meta = video_meta_id[movie.video]
+            logger.debug "#{movie.video}->picture: #{video_info["picture"]}"
+            movie.picture = video_info["picture"]
+            # feedに入っているsourceだとHDじゃないのでwww.facebook.comドメインのembedで使われるurlを使う
+            # logger.debug "#{movie.video}->source: #{video_info["source"]}"
+            # movie.source = video_info["source"]
+            logger.debug "#{movie.video}->source: http://www.facebook.com/v/#{movie.video}"
+            movie.source = "http://www.facebook.com/v/#{movie.video}"
+            # 動画の長さ以外に何が入るのかよくわかっていない
+            logger.debug "#{movie.video}->length: #{video_info["properties"][0]["text"]}"
+            movie.length = video_info["properties"][0]["text"]
+            movie.save
+          end
         end
       end
     end
+    ##### 以上が暫定対処
 
     respond_to do |format|
       format.html # index.html.erb
@@ -65,7 +82,9 @@ class MoviesController < ApplicationController
   # GET /movies/new.json
   def new
     @movie = Movie.new
+    # fbにajaxでpostするのでaccess_tokenを渡してやる必要がある
     current_user = Omniuser.find(session[:user_id])
+    # gonはjsに変数を渡せる便利gem。下記の場合js側で使う時もgon.tokenでOK
     gon.token = current_user.access_token
 
     respond_to do |format|
