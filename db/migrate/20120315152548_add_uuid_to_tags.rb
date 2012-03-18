ff2d04a0d5ffb9a50ff245d6f014f61faaaa22a5
rynqs <cecilcat@gmail.com>
2012-03-18 15:14:22 +0900

    不要なマイグレーションファイル削除

--- db/migrate/20120315152548_add_uuid_to_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddUuidToTags < ActiveRecord::Migration
-  def change
-    add_column :tags, :uuid, :string
-  end
-end
--- db/migrate/20120317052412_remove_uuid_form_tag.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUuidFormTag < ActiveRecord::Migration
-  def change
-    remove_column :tags, :uuid, :string
-  end
-end
