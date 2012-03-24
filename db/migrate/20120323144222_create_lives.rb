b79181a47f0fe2e5c875d2fad074db8b3b10edd2
rynqs <cecilcat@gmail.com>
2012-03-24 23:45:24 +0900

    liveはconcertに変更

--- db/migrate/20120323144222_create_lives.rb
+++
@@ -1,9 +0,0 @@
-class CreateLives < ActiveRecord::Migration
-  def change
-    create_table :lives do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20120323144416_add_live_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class AddLiveToMovies < ActiveRecord::Migration
-  def change
-    add_column :movies, :live_id, :integer
-  end
-end
