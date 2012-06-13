15784604bf9efae8b59cad35234331cfd4b1913a
rynqs <cecilcat@gmail.com>
2012-06-13 22:08:54 +0900

    タイムゾーンを東京に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ MoogleApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # TimeZone
+  config.time_zone = 'Tokyo'
 end
